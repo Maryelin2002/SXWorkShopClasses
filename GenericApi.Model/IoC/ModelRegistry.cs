bcb9b7f94301326ab18f03c29bcfd37d42657f3e
Yelincode <yelincode@gmail.com>
2021-07-20 08:42:44 -0400

    Initial commit

---
+++ GenericApi.Model/IoC/ModelRegistry.cs
@@ -0,0 +1,17 @@
+﻿using GenericApi.Model.Contexts;
+using GenericApi.Model.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericApi.Model.IoC
+{
+    public static class ModelRegistry
+    {
+        public static void AddModelRegistry(this IServiceCollection services)
+        {
+            services.AddScoped<IDocumentRepository,DocumentRepository>();
+        }
+    }
+}
