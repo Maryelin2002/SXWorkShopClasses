217e838966e1465f60216c11c0a5a772577e9a7f
Maryelin2002 <64933036+Maryelin2002@users.noreply.github.com>
2021-07-21 09:45:45 -0400

    Commit

--- GenericApi.Bl/Mapper/MappingProfile.cs
+++ GenericApi.Bl/Mapper/MappingProfile.cs
@@ -17,11 +17,18 @@ namespace GenericApi.Bl.Mapper
 
             CreateMap<Member, MemberDto>()
                 .ForMember(dto => dto.PhotoFileName, config => config.MapFrom(entity => entity.Photo.FileName));
-
             CreateMap<MemberDto, Member>();
+
             CreateMap<WorkShop, WorkShopDto>().ReverseMap();
-            CreateMap<WorkShopMember, WorkShopMemberDto>().ReverseMap();
-            CreateMap<WorkShopDay, WorkShopDayDto>().ReverseMap();
+
+            CreateMap<WorkShopMember, WorkShopMemberDto>()
+                .ForMember(dto => dto.Member, config => config.MapFrom(entity => entity.Member))
+                .ForMember(dto => dto.WorkShop, config => config.MapFrom(entity => entity.WorkShop));
+            CreateMap<WorkShopMemberDto, WorkShopMember>();
+
+            CreateMap<WorkShopDay, WorkShopDayDto>()
+                .ForMember(dto => dto.WorkShopId, config => config.MapFrom(entity => entity.WorkShopId));
+            CreateMap<WorkShopDayDto, WorkShopDay>();
         }
     }
 }
