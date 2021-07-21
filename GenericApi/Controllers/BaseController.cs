fe27f42d05800d063618a066d3570e01b5eafaa4
Maryelin2002 <64933036+Maryelin2002@users.noreply.github.com>
2021-07-21 12:31:15 -0400

    Commit

---
+++ GenericApi/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using GenericApi.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseController<TEntity, TDto> : ControllerBase
+    {
+
+    }
+}
---
+++ GenericApi/Controllers/DocumentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentController : ControllerBase
+    {
+        
+    }
+}
